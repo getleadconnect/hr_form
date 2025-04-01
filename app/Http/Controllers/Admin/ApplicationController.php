eb34f3b5cfdbcd5c6151357cd739b64f0cdd6506
shajipr21 <shajipr21@gmail.com>
2025-04-01 15:04:50 +0530

    changes- space account

--- app/Http/Controllers/Admin/ApplicationController.php
+++ app/Http/Controllers/Admin/ApplicationController.php
@@ -123,9 +123,9 @@ public function destroy($id)
 		if($app)
 		{
 			if($app->photo!='')
-				Storage::disk('spaces')->delete($users->photo);
+				Storage::disk('spaces')->delete($app->photo);
 			if($app->cv_file!='')
-				Storage::disk('spaces')->delete($users->cv_file);
+				Storage::disk('spaces')->delete($app->cv_file);
 			
 			$res=$app->delete();
 			if($res)
